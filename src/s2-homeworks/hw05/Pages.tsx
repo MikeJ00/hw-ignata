39c90330f7ad9d11bee774827d4d1a64ffbcb319
Mike-casper <misha7709123@gmail.com>
2023-03-03 16:49:31 +0300

    done hw 5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,16 +16,21 @@ function Pages() {
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
+                <Route path={"/"} element={<Navigate to={"/pre-junior"}/>}/>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 {/*<Route ...*/}
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
+                <Route path={"/pre-junior"} element={<PreJunior/>}/>
+                <Route path={"/junior"} element={<Junior/>}/>
+                <Route path={"/junior-plus"} element={<JuniorPlus/>}/>
                 {/*<Route ...*/}
                 {/*<Route ...*/}
                 {/*<Route ...*/}
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+                <Route path={"/*"} element={<div>Error404</div>}/>
                 {/*<Route ...*/}
             </Routes>
         </div>
